d80bceb8e298a2c49bee6ea06bcb4a11dfb066f6
bj-techmaster <123358775+bj-techmaster@users.noreply.github.com>
2023-09-08 12:37:55 +0530

    Update ClaimController.java

--- Spring-boot-insurance-management-platform/src/main/java/com/lic/Springbootinsurancemanagementplatform/controller/ClaimController.java
+++ Spring-boot-insurance-management-platform/src/main/java/com/lic/Springbootinsurancemanagementplatform/controller/ClaimController.java
@@ -2,6 +2,8 @@ package com.lic.Springbootinsurancemanagementplatform.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,35 +20,36 @@ import com.lic.Springbootinsurancemanagementplatform.service.ClaimService;
 @RequestMapping("/api")
 public class ClaimController {
 
-	@Autowired
-	private ClaimService claimService;
-	
-	//insert Claim---------------------------------------------------------------------
-	@PostMapping("/saveClaim/{policyId}")
-	public ResponseStructure<Claim> insertClaim(@RequestBody Claim claim,@PathVariable int policyId) {
-		return claimService.insertClaim(claim, policyId);
-	}	
-	
-	// getByClaimId-----------------------------------------------------------------------------
-	@GetMapping("/getByClaimId/{claimId}")
-	public ResponseStructure<Claim> getByClaimId(@PathVariable int claimId) {
-		return claimService.getByClaimId(claimId);
-	}
-	
-	// delete Claim-----------------------------------------------------------------------------
-	@DeleteMapping("/deleteClaim/{claimId}")
-	public ResponseStructure<Claim> deleteClaim(Claim claim, @PathVariable int claimId) {
-		return claimService.deleteClaim(claim, claimId);
-	}	
-	
-	// update Claim------------------------------------------------------------------------------
-	@PutMapping("/updateClaim/{claimId}")
-	public ResponseStructure<Claim> updateClaim(@RequestBody Claim claim,@PathVariable int claimId) {
-		return claimService.updateClaim(claim, claimId);
-	}	
-	//displayAllClaim----------------------------------------------------------------------------
-	@GetMapping("/displayAllClaim")
-	public ResponseStructure<List<Claim>> displayAllClaim(){
-	
-	}	
+    @Autowired
+    private ClaimService claimService;
+
+    // insert Claim---------------------------------------------------------------------
+    @PostMapping("/saveClaim/{policyId}")
+    public ResponseStructure<Claim> insertClaim(@RequestBody Claim claim, @PathVariable int policyId) {
+        return claimService.insertClaim(claim, policyId);
+    }
+
+    // getByClaimId-----------------------------------------------------------------------------
+    @GetMapping("/getByClaimId/{claimId}")
+    public ResponseStructure<Claim> getByClaimId(@PathVariable int claimId) {
+        return claimService.getByClaimId(claimId);
+    }
+
+    // delete Claim-----------------------------------------------------------------------------
+    @DeleteMapping("/deleteClaim/{claimId}")
+    public ResponseStructure<Claim> deleteClaim(@PathVariable int claimId) {
+        return claimService.deleteClaim(claimId);
+    }
+
+    // update Claim------------------------------------------------------------------------------
+    @PutMapping("/updateClaim/{claimId}")
+    public ResponseStructure<Claim> updateClaim(@RequestBody Claim claim, @PathVariable int claimId) {
+        return claimService.updateClaim(claim, claimId);
+    }
+
+    // displayAllClaim----------------------------------------------------------------------------
+    @GetMapping("/displayAllClaim")
+    public ResponseStructure<List<Claim>> displayAllClaim() {
+        return claimService.getAllClaims();
+    }
 }
